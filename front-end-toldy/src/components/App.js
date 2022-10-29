a78afe06a92422c7ab5c5b695b975abf0aadc3f6
Francisco Toldy <62015529+ftoldy1415@users.noreply.github.com>
2022-10-30 00:55:25 +0100

    front-end work in progress
    Página de perfil tá a funcionar, é preciso completar o css para a página de login

---
+++ front-end-toldy/src/components/App.js
@@ -0,0 +1,17 @@
+import logo from '../images/logo.svg';
+import '../styles/App.css';
+import LogSquare from './LogSquare.js';
+import PopUp from './PopUp.js'
+import Profile from '../pages/Profile.js'
+import AddSpecialist from '../pages/AddSpecialist.js'
+
+function App() {
+  return (
+    <div >
+      <AddSpecialist/>
+    </div>
+    
+	);
+}
+
+export default App;
