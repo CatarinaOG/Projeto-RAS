18744e8c84e1c80446afb6637d61e76198457f02
Francisco Toldy <chicomartinho8@gmail.com>
2022-11-16 20:14:46 +0000

    Historico de transações adicionado

--- front-end/src/components/BetHistory.jsx
+++ front-end/src/components/BetHistory.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import goBack from "../images/goBack.png"
 import BetHistoryBox from './BetHistoryBox'
 
@@ -5,9 +6,27 @@ export default function BetHistory(props){
 
     const {setDivChoice} = props
 
+    const [typeData, setTypeData] = useState('Bet');
+
+    const [multipleState,setMultipleState] = useState('inactive');
+    const [simpleState,setSimpleState] = useState("inactive");
+
     //comportamento do botão para voltar para tras
     function goToData(){
         setDivChoice("Data");
+    }
+    function goToDataToBet(){
+        setTypeData("Bet")
+    }
+
+    function changeSimpleState(){
+        setMultipleState('inactive');
+        setSimpleState('active');
+    }
+
+    function changeMultState(){
+      setMultipleState('active');
+      setSimpleState('inactive');
     }
 
 
@@ -36,16 +55,76 @@ export default function BetHistory(props){
           })}
         </ul>)
 
+
+      const data = [
+        { date: "18/11/22", description: "Levantamento", operation: "+13,45" , balance : 20},
+        { date: "18/11/22", description: "Depósito", operation: "+13,45" , balance : 32},
+        { date: "18/11/22", description: "Ganho de aposta", operation: "+13,45", balance : 50 },
+        { date: "18/11/22", description: "Ganho de aposta", operation: "+13,45", balance : 50 },
+        { date: "18/11/22", description: "Ganho de aposta", operation: "+13,45", balance : 50 },
+        { date: "18/11/22", description: "Ganho de aposta", operation: "+13,45", balance : 50 },
+        { date: "18/11/22", description: "Ganho de aposta", operation: "+13,45", balance : 50 },
+        { date: "18/11/22", description: "Ganho de aposta", operation: "+13,45", balance : 50 },
+        { date: "18/11/22", description: "Ganho de aposta", operation: "+13,45", balance : 50 },
+        { date: "18/11/22", description: "Ganho de aposta", operation: "+13,45", balance : 50 },
+        { date: "18/11/22", description: "Ganho de aposta", operation: "+13,45", balance : 50 },
+        { date: "18/11/22", description: "Ganho de aposta", operation: "+13,45", balance : 50 },
+        { date: "18/11/22", description: "Ganho de aposta", operation: "+13,45", balance : 50 },
+        { date: "18/11/22", description: "Aposta feita", operation: "+13,45", balance : 50 }
+      ]
+  
+
+    function changeToTransact(){
+      setMultipleState('inactive')
+      setSimpleState('inactive')
+
+      setTypeData("Transact")
+    }
+
     return(
+      
       <div>
+        { typeData === 'Bet' &&
+        <div>
         <div className="buttonHistoryDiv">
-           <button className='ftsimpleBet'>Simples</button>
-           <button className='ftmultipleBet'>Múltiplas</button>
+           <button className={ simpleState ==='inactive' ? "ftsimpleBet" : "ftsimpleBetSelected"} onClick={changeSimpleState}>Simples</button>
+           <button className={ multipleState ==='inactive' ? "ftmultipleBet" : "ftmultipleBetSelected"} onClick={changeMultState}>Múltiplas</button>
            <img onClick={goToData} src = {goBack} className="ftgoBack"/>
+           <button  className='ftChangeToTransact' onClick={changeToTransact} > {'>'} </button>
         </div>
         <div className="betHistoryDiv">
               <List></List>
         </div>
         </div>
+      }
+       {typeData === 'Transact' &&
+       <div>
+        <div className="buttonHistoryDiv">
+          <img onClick={goToDataToBet} src = {goBack} className="ftgoBack"/>
+        </div>
+        <div className="tableDiv">
+          <table>
+            <tr>
+              <th>Data</th>
+              <th>Descrição</th>
+              <th>Operação</th>
+              <th>Saldo Após Transação</th>
+            </tr>
+            {data.map((val, key) => {
+              return (
+                <tr key={key}>
+                  <td>{val.date}</td>
+                  <td>{val.description}</td>
+                  <td>{val.operation}</td>
+                  <td>{val.balance}</td>
+                </tr>
+              )
+            })}
+          </table>
+
+        </div> 
+       </div>
+       }   
+      </div>
     )
 }
