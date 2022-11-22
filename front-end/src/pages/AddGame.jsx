bfc70267b8a39bba85783d7796edd80dd0193083
Francisco Toldy <chicomartinho8@gmail.com>
2022-11-22 15:47:49 +0000

    motogp com lista de pilotos em componente separado

--- front-end/src/pages/AddGame.jsx
+++ front-end/src/pages/AddGame.jsx
@@ -5,11 +5,13 @@ import goBackImg from '../images/goBack.png'
 
 import NavBarProfile from "../components/NavBarProfile"
 import PopUpAddGame from '../components/PopUpAddGame'
+import PilotsForm from '../components/PilotsForm';
 
 
 export default function AddGame(props){
 
     const {username,email} = props
+    const [pilotError,setPilotError] = useState(0);
 
     const [formData, setFormData] = useState(
         {sport: "",
@@ -77,10 +79,22 @@ export default function AddGame(props){
         })
     }
 
+    function checkFormPilot(){
+        if(formData.pilot1==="" || formData.pilot2==="" || formData.pilot3==="" || formData.pilot4==="" || formData.pilot5==="" || formData.pilot6==="" ||
+        formData.pilot7==="" || formData.pilot8==="" || formData.pilot9==="" || formData.pilot10==="" || formData.pilot11==="" || formData.pilot12==="" ||
+        formData.pilot13==="" || formData.pilot14==="" || formData.pilot15==="" || formData.pilot16==="" || formData.pilot17==="" || formData.pilot8==="" ||
+        formData.pilot19==="" || formData.pilot20==="" || formData.pilot21==="" || formData.pilot22==="" || formData.pilot23==="" || formData.pilot24===""){
+            setPilotError(1);
+        }
+        else{
+            setPilotError(0)
+        }
+    }
+
 	function handleSubmit(event){
 		event.preventDefault()
 
-        if(formData.sport!="" && formData.participantA!="" && formData.participantB!="" && formData.date!="" && formData.time!=""){
+        if(formData.sport!="motoGP" && formData.participantA!="" && formData.participantB!="" && formData.date!="" && formData.time!=""){
             const timeVal = formData.date + "T"+formData.time+":00"
 
             fetch('http://127.0.0.1:8080/api/expert/newGame', {
@@ -107,6 +121,25 @@ export default function AddGame(props){
                 }
             })
         }
+        else if(formData.sport==="motoGP"){
+            checkFormPilot();
+            if(pilotError===0){
+                fetch('http://127.0.0.1:8080/api/expert/race', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json ',
+                    },
+                    body: motoBody
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.state === 'good'){
+                        setConfirmed(true)
+                        setErrorReg(0)
+                    }
+                })  
+            }
+        }
         else{
             setErrorReg(2)
         }
@@ -127,136 +160,13 @@ export default function AddGame(props){
         </div>
             );
     }
-    */
+    
 
+    
     const pilotList = (
-        <div className='divPilots'>
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 1' name = "pilot1" value = {formData.pilot1}/>
-                <input className='ftoddPilot' type="number" min="0" onChange={handleChange} placeholder = "Odd 1" name="odd1" value = {formData.odd1}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 2' name = "pilot2" value = {formData.pilot2}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 2" name="odd2" value = {formData.odd2}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 3' name = "pilot3" value = {formData.pilot3}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 3" name="odd3" value = {formData.odd3}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 4' name = "pilot4" value = {formData.pilot4}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 4" name="odd4" value = {formData.odd4}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 5' name = "pilot5" value = {formData.pilot5}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 5" name="odd5" value = {formData.odd5}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 6' name = "pilot6" value = {formData.pilot6}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 6" name="odd6" value = {formData.odd6}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 7' name = "pilot7" value = {formData.pilot7}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 7" name="odd7" value = {formData.odd7}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 8' name = "pilot8" value = {formData.pilot8}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 8" name="odd8" value = {formData.odd8}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 9' name = "pilot9" value = {formData.pilot9}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 9" name="odd9" value = {formData.odd9}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 10' name = "pilot10" value = {formData.pilot10}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 10" name="odd10" value = {formData.odd10}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 11' name = "pilot11" value = {formData.pilot11}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 11" name="odd11" value = {formData.odd11}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 12' name = "pilot12" value = {formData.pilot12}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 12" name="odd12" value = {formData.odd12}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 13' name = "pilot13" value = {formData.pilot13}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 13" name="odd13" value = {formData.odd13}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 14' name = "pilot14" value = {formData.pilot14}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 14" name="odd14" value = {formData.odd14}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 15' name = "pilot15" value = {formData.pilot15}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 15" name="odd15" value = {formData.odd15}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 15' name = "pilot15" value = {formData.pilot15}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 15" name="odd15" value = {formData.odd15}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 16' name = "pilot1" value = {formData.pilot16}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 16" name="odd16" value = {formData.odd16}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 17' name = "pilot17" value = {formData.pilot17}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 17" name="odd17" value = {formData.odd17}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 18' name = "pilot18" value = {formData.pilot18}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 18" name="odd18" value = {formData.odd18}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 19' name = "pilot19" value = {formData.pilot19}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 19" name="odd19" value = {formData.odd19}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 20' name = "pilot20" value = {formData.pilot20}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 20" name="odd20" value = {formData.odd20}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 21' name = "pilot21" value = {formData.pilot21}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 21" name="odd21" value = {formData.odd21}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 22' name = "pilot22" value = {formData.pilot22}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 22" name="odd22" value = {formData.odd22}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 23' name = "pilot23" value = {formData.pilot23}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 23" name="odd23" value = {formData.odd23}/>
-            </div>
-
-            <div className='pilotOdd'>
-                <input className ="ftChoosePilot" type="text" onChange={handleChange}  placeholder='Pilot 24' name = "pilot24" value = {formData.pilot24}/>
-				<input className='ftoddPilot' type="number" onChange={handleChange} placeholder = "Odd 24" name="odd24" value = {formData.odd24}/>
-            </div>
-        </div>
+        
     )
+    */
 
     return(
         <div>
@@ -304,7 +214,7 @@ export default function AddGame(props){
                             <input className ="ftselectPartA" type="text" onChange={handleChange}  placeholder='Event Name' name = "raceName" value = {formData.raceName}/>
 
                             <h3 className='ftpromptEvent'>Insira o nome dos pilotos e respectiva odd</h3>
-                            {pilotList}
+                            <PilotsForm formData={formData} handleChange={handleChange} ></PilotsForm>
                         </div>}
 
 
